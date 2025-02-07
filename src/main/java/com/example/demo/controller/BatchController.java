c1edefe71b588c1f7e10db8b36cd41c199da81d1
mooncw <dusrktl4@gmail.com>
2025-02-08 00:50:21 +0900

    [refactor] 실행 시간 로그 어노테이션 적용

--- src/main/java/com/example/demo/controller/BatchController.java
+++ src/main/java/com/example/demo/controller/BatchController.java
@@ -1,9 +1,9 @@
 package com.example.demo.controller;
 
+import com.example.demo.annotation.LogExecutionTime;
 import com.example.demo.service.BatchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +18,6 @@ public class BatchController {
 
     private final BatchService batchService;
 
-    // 시간 측정을 위한 객체
-    private final StopWatch stopWatch = new StopWatch();
-
     // 사용하기 쉬운 Get 사용
     @GetMapping("/delete")
     public String delete() {
@@ -29,46 +26,38 @@ public class BatchController {
     }
 
     // 사용하기 쉬운 Get 사용
+    @LogExecutionTime(value = "individual insert")
     @GetMapping("/insert/{num}")
     public String insert(
         @PathVariable Long num
     ) {
-        stopWatch.start();
         batchService.insert(num);
-        stopWatch.stop();
-        log.info(">>> 개별 insert {}개 걸린 시간: {} ms", num, stopWatch.getTotalTimeMillis());
         return "insert OK";
     }
 
     // 사용하기 쉬운 Get 사용
+    @LogExecutionTime(value = "batch insert")
     @GetMapping("/insert/batch/{num}")
     public String batchInsert(
         @PathVariable Long num
     ) {
-        stopWatch.start();
         batchService.batchInsert(num);
-        stopWatch.stop();
-        log.info(">>> batch insert {}개 걸린 시간: {} ms", num, stopWatch.getTotalTimeMillis());
         return "batch insert OK";
     }
 
     // 사용하기 쉬운 Get 사용
+    @LogExecutionTime(value = "individual update")
     @GetMapping("/update")
     public String update() {
-        stopWatch.start();
         batchService.update();
-        stopWatch.stop();
-        log.info(">>> 개별 update 100000개 걸린 시간: {} ms", stopWatch.getTotalTimeMillis());
         return "update OK";
     }
 
     // 사용하기 쉬운 Get 사용
+    @LogExecutionTime(value = "batch update")
     @GetMapping("/update/batch")
     public String batchUpdate() {
-        stopWatch.start();
         batchService.batchUpdate();
-        stopWatch.stop();
-        log.info(">>> batch update 100000개 걸린 시간: {} ms", stopWatch.getTotalTimeMillis());
         return "batch update OK";
     }
 }
